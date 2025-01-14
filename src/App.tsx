3daaca5d5bc9a7cebb51bb083bcdfc82b4ced6a9
YanGraim <yangraim@gmail.com>
2025-01-14 14:51:18 -0300

    renderizando componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ export default function App() {
   return (
     <div>
       <h1>Meu projeto</h1>
+      <Aluno/>
     </div>
+  )
+}
+
+function Aluno() {
+  return (
+    <h1>Aluno: Yan Graim</h1>
   )
 }
